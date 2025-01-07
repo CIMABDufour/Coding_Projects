2df1f536d38bd4d1fe2c8aeb5c64f9f8bbaf9fa8
Cima_B_Dufour <56842615+CIMABDufour@users.noreply.github.com>
2025-01-06 22:00:07 -0500

    Create PFM_Main.py

---
+++ Python/02_03_Personnal_Finance_Manager/PFM_Main.py
@@ -0,0 +1,17 @@
+# Project Difficulty : Medium
+# Personnal Finance Manager
+# This file execute the main function
+
+
+print("Hello")
+
+
+# Defining main function
+def main():
+    print("hey there")
+
+
+# Using the special variable
+# __name__
+if __name__ == "__main__":
+    main()
