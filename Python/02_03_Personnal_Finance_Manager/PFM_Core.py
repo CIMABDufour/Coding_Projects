54e439404d03f6692faea34103ea2a144dae1ec1
Cima_B_Dufour <56842615+CIMABDufour@users.noreply.github.com>
2025-01-07 11:58:59 -0500

    Update PFM_Core.py

--- Python/02_03_Personnal_Finance_Manager/PFM_Core.py
+++ Python/02_03_Personnal_Finance_Manager/PFM_Core.py
@@ -1,5 +1,18 @@
 # Project Difficulty : Medium
-# Personnal Finance Manager
+# Personal Finance Manager
 # This file contain the core functions of the tool
 
-print('core_function_file')
+import json
+
+
+class User :
+    def __init__(self, first_name, last_name, password) :
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password
+
+
+class Expenses :
+    def __init__(self, category_name, cost) :
+        self.category_name = category_name
+        self.cost = cost
